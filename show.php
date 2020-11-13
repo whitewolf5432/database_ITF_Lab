ba27d1f0645f989563684f0f7a314ae4fbe9cd99
White wolf <59527394+whitewolf5432@users.noreply.github.com>
2020-11-13 12:39:11 +0700

    By me.

---
+++ show.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>Show data in guestbook</title>
+</head>
+<body>
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itflabmysqlserver.mysql.database.azure.com', 'nattapat@itflabmysqlserver', 'Gram25452002++', 'LabITF', 3306)
+if (mysqli_real_connect($conn))
+{
+    die('Failed connecting to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+<table width="700" border="1">
+    <tr>
+        <th width="50"> <div align="center">ID</div></th>
+        <th width="100"> <div align="center">Name</div></th>
+        <th width="400"> <div align="center">Comment</div></th>
+        <th width="150"> <div align="center">Link</div></th>
+    </tr>
+<?php
+while($result = mysqli_fecth_array(&res))
+{
+?>
+    <tr>
+        <td><?php echo $result['id'];?></td>
+        <td><?php echo $result['name'];?></td>
+        <td><?php echo $result['comment'];?></td>
+        <td><?php echo $result['link'];?></td>
+    </tr>
+<?php
+}
+?>
+</table>
+<?php mysqli_close($conn); ?>
+</body>
+</html>
