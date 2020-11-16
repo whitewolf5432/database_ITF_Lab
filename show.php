05ac91551a73e08954c7ae3b65bd72184b35c519
White wolf <59527394+whitewolf5432@users.noreply.github.com>
2020-11-16 22:32:43 +0700

    By me.

--- show.php
+++ show.php
@@ -1,34 +1,32 @@
 <html>
 <head>
-<title>Show data in guestbook</title>
+<title>ITF Lab</title>
 </head>
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'itflabmysqlserver.mysql.database.azure.com', 'nattapat@itflabmysqlserver', 'Gram25452002++', 'LabITF', 3306)
-if (mysqli_real_connect($conn))
+mysqli_real_connect($conn, 'itflabmysqlserver.mysql.database.azure.com', 'nattapat@itflabmysqlserver', 'Gram25452002++', 'labitf', 3306);
+if (mysqli_connect_errno($conn))
 {
-    die('Failed connecting to MySQL: '.mysqli_connect_error());
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="700" border="1">
-    <tr>
-        <th width="50"> <div align="center">ID</div></th>
-        <th width="100"> <div align="center">Name</div></th>
-        <th width="400"> <div align="center">Comment</div></th>
-        <th width="150"> <div align="center">Link</div></th>
-    </tr>
+<table width="600" border="1">
+  <tr>
+    <th width="100"> <div align="center">Name</div></th>
+    <th width="350"> <div align="center">Comment </div></th>
+    <th width="150"> <div align="center">Link </div></th>
+  </tr>
 <?php
-while($Result = mysqli_fecth_array(&res))
+while($Result = mysqli_fetch_array($res))
 {
 ?>
-    <tr>
-        <td><?php echo $Result['id'];?></td>
-        <td><?php echo $Result['name'];?></td>
-        <td><?php echo $Result['comment'];?></td>
-        <td><?php echo $Result['link'];?></td>
-    </tr>
+  <tr>
+    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Comment'];?></td>
+    <td><?php echo $Result['Link'];?></td>
+  </tr>
 <?php
 }
 ?>
