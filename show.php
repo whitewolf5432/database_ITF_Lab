4235d6156ff113cbcba7972adb738b70595e6241
White wolf <59527394+whitewolf5432@users.noreply.github.com>
2020-11-16 22:37:00 +0700

    Update show.php

--- show.php
+++ show.php
@@ -12,10 +12,11 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
+<table width="700" border="1">
   <tr>
+    <th width="50"> <div align="center">ID</div></th>
     <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
+    <th width="400"> <div align="center">Comment </div></th>
     <th width="150"> <div align="center">Link </div></th>
   </tr>
 <?php
@@ -23,9 +24,10 @@ while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
+    <td><?php echo $Result['id'];?></td>
+    <td><?php echo $Result['name'];?></td>
+    <td><?php echo $Result['comment'];?></td>
+    <td><?php echo $Result['link'];?></td>
   </tr>
 <?php
 }
